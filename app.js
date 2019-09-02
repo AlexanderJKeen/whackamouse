2e55ba29f7d5bac919b51d576710c2097d9f91b7
CN138 <cn138@CN0138.local>
2019-09-02 15:54:17 +0100

    intialUpload

---
+++ app.js
@@ -0,0 +1,9 @@
+const holes = document.querySelectorAll('.hole');
+const scoreBoard = document.querySelector('.score');
+const moles = document.querySelectorAll('.mole');
+
+let images = [`badGuy2.png`,`badGuy3.png`,`badGuy4.png`,`Hero.png`,`Hero2.png`,`Hero3.png`
+
+
+	
+
