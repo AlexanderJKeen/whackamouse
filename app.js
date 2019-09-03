886d753d8c526e31d3636194b0d08d9f44db75f6
CN138 <cn138@CN0138.local>
2019-09-03 16:47:35 +0100

    revamp

--- app.js
+++ app.js
@@ -1,50 +1,21 @@
-const holes = document.querySelectorAll('.hole');
-const scoreBoard = document.querySelector('.score');
-const moles = document.querySelectorAll('.mole');
-let go = document.getElementById('startGames')
-let endGame = false;
-
-let randomTime = (min, max) =>{
-return Math.floor(Math.random() * (max -min)+ min);
+const field = document.getElementsByClassName('field')
+const total1 = document.getElementById('score');
+const hero = document.getElementsByClassName('mrbump')
+const image = [`./badGuy.png`,`./mrbump.png`]
+let total = 0;
+let random = Math.floor(Math.random * 6)
+for(i =0;i<hero.length;i++){
+hero[i].addEventListener("click", event1 => {
+    total = total +=1 
+    total1.textContent = total
+})
 }
-
-let randomField = (holes) => {
-    const num = Math.floor(Math.random() * holes.length);
-    const hole = holes[num];
-}
-
-const bob = () =>{
-    const time = randomeTime(200,1000)
-    const hole = randomHole(holes)
-    hole.classList.add('up')
-    setTimeout(() => {
-        hole.classList.remove('up');
-        if (!endGame) 
-        bob();
-    }, time);
-}
-let startGame = () =>{
-    scoreBoard.textContent = 0;
-    endGame = fales;
-    score = 0;
-    bob();
-    setTimeout (() => endGame = true, 3000)
-}
-let hit = () => {
-    score++
-    this.classList.remove('up');
-    scoreBoard.textContent = score;
-}
-go.addEventListener("click", startGame());
-
-mice.forEach(micky => micky.addEventListener("click", hit));
-
-// let images = [`badGuy2.png`,`badGuy3.png`,`badGuy4.png`,`Hero.png`,`Hero2.png`,`Hero3.png`]
-
-// for(i=0;i<images.length;i++){
-// let random = Math.floor(Math.random() * images[i])
-// image.src = `./img/${images[random]}`
-// }
-
-	
-
+// let random = Math.floor(Math.random() * 6)
+//     image.url = `./img/${images[random]}`
+//     if (image.url = `./img/${images[0]}`){ 
+// } else if (image.src = `./img/${images[1]}`){
+//     total = total - 1
+//     total1.textContent = total
+//  }
+ // if (total === 4){
+    //     hero[i].removeEventListener("click", event1)
